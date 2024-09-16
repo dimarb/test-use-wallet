61b89d5738639eba9de29df550223ca16d22c119
Dimar Borda <dimarborda@MacBook-Air-de-Dimar.local>
2024-09-16 12:28:30 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { NetworkId, WalletId, WalletManager, WalletProvider } from '@txnlab/use-wallet-react'
+import ReactDOM from 'react-dom/client'
+import MyApp from './MyApp'
+import SendAlgos from './SendAlgos'
+// Create a manager instance
+const walletManager = new WalletManager({
+  wallets: [ WalletId.PERA ],
+  network: NetworkId.TESTNET
+})
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  return (
+    // Provide the manager to your App
+    <WalletProvider manager={walletManager}>
+      <MyApp />
+      <SendAlgos />
+    </WalletProvider>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
